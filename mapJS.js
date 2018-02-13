9762adfe9a72631b021bcd3ddd43f45a245b7e65
philippezhang <32865596+philippezhang@users.noreply.github.com>
2018-02-13 12:30:13 +0100

    Update mapJS.js

--- mapJS.js
+++ mapJS.js
@@ -13,6 +13,88 @@ var Stamen_TonerBackground = L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.
 
 var map = L.map('map',{layers : Stamen_TonerBackground}).setView([20.858376, 5.294442],2);
 
+
+		//Rendre draggable les div des pays
+	$( "#France" ).draggable({ revert: "valid" });
+	$( "#Canada" ).draggable({ revert: "valid" });
+	$( "#Italie" ).draggable({ revert: "valid" });
+	$( "#Belgique" ).draggable({ revert: "valid" });
+	$( "#Japan" ).draggable({ revert: "valid" });
+	
+	//Rendre la map droppable
+	 $( "#map" ).droppable({
+		 
+		 //Evenement lors du drop
+		drop: function( event, ui ) {
+			
+			//Recupère l'id du block div "dropped" dans la map
+			var IdPays = ui.draggable.attr("id");
+			
+			var chaine="";
+			chaine+="Pays : "+IdPays+"</br>";
+			
+			//Requete AJAX pour récupérer les coordonnées (lati, longi) du pays
+			$.ajax({
+			    type: 'GET',
+			    url: "http://nominatim.openstreetmap.org/search",
+			    dataType: 'jsonp',
+			    jsonpCallback: 'data',
+			    data: { format: "json", limit: 1,country: IdPays,json_callback: 'data' },
+			    error: function(xhr, status, error) {
+						alert("ERROR "+error);
+			    },
+			    success: function(data){
+				//récupérer les coordonnées (lati, longi) du pays dans les données json provenant du serveur
+					var lati = '';
+					var longi = '';
+					$.each(data, function() {
+						lati = this['lat'] ;
+						longi = this['lon'] ;
+				});
+				
+				//affichage des infos
+				chaine+="Latitude : "+lati+"</br>";
+				chaine+="Longitute : "+longi+"</br>";
+				$( "#info" ).html(chaine);
+				
+				//MAJ de la map à la position (lati, longi) du pays
+				map.panTo(new L.LatLng(lati, longi));		
+				
+			    }
+			});
+			
+			
+		}
+	});
+	
+	//Sur le click de la map, ajout d'un marqueur sur la carte avec le nom du pays
+	map.on('click', onClick);
+	
+	function onClick(e) {
+		//recherche le pays sur lequel on a clické
+		//Requete AJAX pour récupérer les infos du pays sur le point où on a cliqué (lati, longi) 
+		$.ajax({
+		    type: 'GET',
+		    url: "http://nominatim.openstreetmap.org/reverse",
+		    dataType: 'jsonp',
+		    jsonpCallback: 'data',
+		    data: { format: "json", limit: 1,lat: e.latlng.lat,lon: e.latlng.lng,json_callback: 'data' },
+		    error: function(xhr, status, error) {
+					alert("ERROR "+error);
+		    },	  
+			success: 	function (data){
+			
+			//récupérer les coordonnées (lati, longi) du pays dans les données json provenant du serveur
+				var paysVisite ='';
+				paysVisite = data["address"]['country'] ;
+			
+			//affichage des infos
+			L.marker(e.latlng).addTo(map).bindPopup("Lat, Lon : " + e.latlng.lat + ", " + e.latlng.lng+" Pays : "+paysVisite).openPopup();
+			L.circle(e.latlng, 1).addTo(map);	
+			}
+		});
+	}
+	
 }
 
 
