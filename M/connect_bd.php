ad2430f3de25b388c088980739a7edb34409527f
philippezhang <32865596+philippezhang@users.noreply.github.com>
2018-03-13 17:28:00 +0100

    Update connect_bd.php

--- M/connect_bd.php
+++ M/connect_bd.php
@@ -1,18 +1,25 @@
 <?php
 
 
-//chez vous, par exemple avec uwamp
-$hote='localhost';   		
-$login='root';  	
-$pass=''; 		
-$bd='pweb17_cassanel'; //créer préalablement la base de données
+// //chez vous, par exemple avec uwamp
+// $hote='localhost';   		
+// $login='root';  	
+// $pass=''; 		
+// $bd='pweb17_cassanel'; //créer préalablement la base de données
 
 
 //à l'iut
-#$hote="localhost";   		
-#$login="cassanel";  		
-#$pass="cassanel"; 
-#$bd="pweb17_cassanel"; 
+$hote="localhost";   		
+$login="cassanel";  		
+$pass="cassanel"; 
+$bd="pweb17_cassanel"; 
+
+// //joueur de la base
+// login=Sacdeneu mdp=root
+// login=alison mdp=alison
+// login=root= mdp=root
+// login=philippe mdp=philippe
+
 
 if (!isset($link)) {
 $link = mysqli_connect($hote, $login, $pass) 
@@ -21,3 +28,7 @@ mysqli_select_db($link, $bd)
 		or die (htmlentities("erreur d'accès à la base :") . $bd);
 }
 ?>
+
+
+
+
